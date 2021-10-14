9a4faf628e695589b58a9b1bba4ae6defb1f0e95
ThatAvocado_Boi <sir4alterno@gmail.com>
2021-10-15 01:08:10 +0300

    Update #3

--- prak1/prak1.cpp
+++ prak1/prak1.cpp
@@ -13,8 +13,57 @@ int main()
     std::cout << sizeof(long double) << ' ';
     std::cout << sizeof(char) << ' ';
     std::cout << sizeof(bool) << ' ';
-std:cout << endl;
+    std:cout << endl;
 
     std::cout << "Task 2" << ' ';
+
+
+    int value;
+    cin >> value;
+    unsigned int order = sizeof(int) * 8;
+    unsigned int mask = 1 << order - 1;
+    for (i = 1; i <= order; i++)
+    {
+        putchar(value & mask ? '1' : '0');
+        value <<= 1;
+        if (i % 8 == 0)
+        {
+            putchar(' ');
+        }
+        if (i % order - 1 == 0)
+        {
+            putchar(' ');
+        }
+    }
+
+std:cout << endl;
+
+
+
+
+
+
+    std::cout << "Task 3" << ' ';
+
+    union {
+        float pi = 3.14;
+        int a;
+    };
+
+    mask = 1 << order - 1;
+    for (i = 1; i <= order; i++)
+    {
+        putchar(a & mask ? '1' : '0');
+        a <<= 1;
+        if (i % 8 == 0)
+        {
+            putchar(' ');
+        }
+        if (i % order - 1 == 0)
+        {
+            putchar(' ');
+        }
+    }
+    return 0;
 }
 
