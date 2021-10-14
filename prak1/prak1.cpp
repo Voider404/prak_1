e6a45bf91785b2387aeb6f4c10874a742f670a9c
ThatAvocado_Boi <sir4alterno@gmail.com>
2021-10-15 01:26:31 +0300

    Fixed #1 and #2

--- prak1/prak1.cpp
+++ prak1/prak1.cpp
@@ -4,25 +4,24 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Task 1" << ' ';
-    std::cout << sizeof(int) << ' ';
-    std::cout << sizeof(short int) << ' ';
-    std::cout << sizeof(long long int) << ' ';
-    std::cout << sizeof(float) << ' ';
-    std::cout << sizeof(double) << ' ';
-    std::cout << sizeof(long double) << ' ';
-    std::cout << sizeof(char) << ' ';
-    std::cout << sizeof(bool) << ' ';
-    std:cout << endl;
+    cout << "Task 1" << ' ';
+    cout << sizeof(int) << ' ';
+    cout << sizeof(short int) << ' ';
+    cout << sizeof(long long int) << ' ';
+    cout << sizeof(float) << ' ';
+    cout << sizeof(double) << ' ';
+    cout << sizeof(long double) << ' ';
+    cout << sizeof(char) << ' ';
+    cout << sizeof(bool) << ' ';
+    cout << endl;
 
-    std::cout << "Task 2" << ' ';
+    cout << "Task 2" << ' ';
 
 
-    int value;
-    cin >> value;
+    int value = 127;
     unsigned int order = sizeof(int) * 8;
     unsigned int mask = 1 << order - 1;
-    for (i = 1; i <= order; i++)
+    for (int i = 1; i <= order; i++)
     {
         putchar(value & mask ? '1' : '0');
         value <<= 1;
@@ -36,34 +35,6 @@ int main()
         }
     }
 
-std:cout << endl;
-
-
-
-
-
-
-    std::cout << "Task 3" << ' ';
-
-    union {
-        float pi = 3.14;
-        int a;
-    };
-
-    mask = 1 << order - 1;
-    for (i = 1; i <= order; i++)
-    {
-        putchar(a & mask ? '1' : '0');
-        a <<= 1;
-        if (i % 8 == 0)
-        {
-            putchar(' ');
-        }
-        if (i % order - 1 == 0)
-        {
-            putchar(' ');
-        }
-    }
     return 0;
 }
 
