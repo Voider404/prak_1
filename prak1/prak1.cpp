fb5ff1171e676675ffb008e0db3418e0f1edf8ad
ThatAvocado_Boi <sir4alterno@gmail.com>
2021-10-15 01:38:21 +0300

    Adding answers to #3 and #4

--- prak1/prak1.cpp
+++ prak1/prak1.cpp
@@ -15,10 +15,10 @@ int main()
     cout << sizeof(bool) << ' ';
     cout << endl;
 
+
     cout << "Task 2" << ' ';
-
-
-    int value = 127;
+    int value;
+    cin >> value;
     unsigned int order = sizeof(int) * 8;
     unsigned int mask = 1 << order - 1;
     for (int i = 1; i <= order; i++)
@@ -34,7 +34,57 @@ int main()
             putchar(' ');
         }
     }
+    cout << endl;
 
+
+    cout << "Task 3" << ' ';
+
+    union {
+        float pi = 3.14;
+        int a;
+    };
+
+    mask = 1 << order - 1;
+    for (int i = 1; i <= order; i++)
+    {
+        putchar(a & mask ? '1' : '0');
+        a <<= 1;
+        if (i % 8 == 0)
+        {
+            putchar(' ');
+        }
+        if (i % order - 1 == 0)
+        {
+            putchar(' ');
+        }
+    }
+    cout << endl;
+
+
+    cout << "Task 4" << ' ';
+
+    union {
+        double pi_1;
+        int b[2];
+    };
+
+    mask = 1 << order - 1;
+    for (int j=0; j <= 1; j++){
+     for (int i = 1; i <= order; i++)
+        {
+        putchar(a & mask ? '1' : '0');
+        a <<= 1;
+        if (i % 8 == 0)
+        {
+            putchar(' ');
+        }
+        if (i % order - 1 == 0)
+        {
+            putchar(' ');
+        }
+         }
+    }
+    cout << endl;
     return 0;
 }
 
